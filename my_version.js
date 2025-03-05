47ae9898383c9b9ac81bc751c3e7a7d38c26f2db
Jakob Schmid <jakobschmid25.12@gmail.com>
2025-03-05 15:35:25 +0100

    fix text rotation in version number

--- my_version.js
+++ my_version.js
@@ -19,8 +19,15 @@ module.exports = {
     (module VersionNumber (layer F.Cu) (tedit 5B24D78E)
       ${p.at /* parametric position */}
       ${'' /* footprint reference */}
-      (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-      (fp_text value "${p.version}" (at 0 0) (layer ${p.side}.SilkS) (effects (font (size 1.75 1.75) (thickness 0.5))))
+      (fp_text reference "${p.ref}" 
+        (at 0 0) 
+        (layer F.SilkS) 
+        ${p.ref_hide} 
+        (effects (font (size 1.27 1.27) (thickness 0.15))))
+      (fp_text value "${p.version}" 
+        (at 0 0 ${p.rot}) 
+        (layer ${p.side}.SilkS) 
+        (effects (font (size 1.75 1.75) (thickness 0.5))))
     )
     `
 }
